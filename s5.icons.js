c8884861b1d4e1824646e14291146cb169fdc555
S5 <carlos.diaz@sinco.com.co>
2017-10-27 17:28:01 -0500

    Nuevo icono
    Icono de teléfono agregado al js

--- s5.icons.js
+++ s5.icons.js
@@ -1872,6 +1872,24 @@
             _svg.appendChild(_path);
 
             return Sinco.extend(_svg);
+        },
+        Telefono: function (dim, bgColor) {
+            dim = dim || 18;
+
+            var _svg = ce('svg');
+            _svg.attr('width', dim);
+            _svg.attr('height', dim);
+
+            _svg.attr('viewBox', '0 0 18 18');
+
+            var _path = ce('path');
+
+            _path.attr('d', 'M3.6,7.8 C5,10.6 7.4,12.9 10.2,14.4 L12.4,12.2 C12.7,11.9 13.1,11.8 13.4,12 C14.5,12.4 15.7,12.6 17,12.6 C17.6,12.6 18,13 18,13.6 L18,17 C18,17.6 17.6,18 17,18 C7.6,18 0,10.4 0,1 C0,0.4 0.4,0 1,0 L4.5,0 C5.1,0 5.5,0.4 5.5,1 C5.5,2.2 5.7,3.4 6.1,4.6 C6.2,4.9 6.1,5.3 5.9,5.6 L3.6,7.8 L3.6,7.8 Z');
+            _path.style.strokeWidth = 0;
+            _path.style.fill = bgColor || '#1B344C';
+            _svg.appendChild(_path);
+
+            return Sinco.extend(_svg);
         }
     };
 
